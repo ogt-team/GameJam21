508df10fb2b8b5660167fc27308e3034ce922b0d
Rene Müller <rene.mueller.md@googlemail.com>
2021-04-12 21:15:12 +0200

    add cEngine to Test-Game

--- 001_Tester/main.js
+++ 001_Tester/main.js
@@ -1,1 +1,21 @@
-alert("Test :P")
+let player = {
+    x: 0,
+    y: 0
+  },
+  engine = cEngine.create({
+    autoClear: true,
+    plugins: {
+        input: cEngine.input.create()
+    },
+    step: (context) => {
+      if (engine.plugins.input.keys.W) player.y--
+      if (engine.plugins.input.keys.A) player.x--
+      if (engine.plugins.input.keys.S) player.y++
+      if (engine.plugins.input.keys.D) player.x++
+
+      context.fillStyle = 'red'
+      context.fillRect(player.x, player.y, 10, 10)
+    }
+  })
+
+engine.start()
